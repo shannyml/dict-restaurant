482056eae2cf2e870175ab34f7871fd429d2d0ee
shannen yap <shannen.yml@gmail.com>
2022-10-03 11:47:12 -0700

    Completed reading ratings from a file

--- ratings.py
+++ ratings.py
@@ -2,3 +2,23 @@
 
 
 # put your code here
+
+read_file = open("scores.txt")
+dictionary_restaurant = {}
+
+for line in read_file:
+    line = line.rstrip()
+    line = line.split(":")
+    restaurant_name = line[0]
+    rate = line[1]
+
+    dictionary_restaurant[restaurant_name] = rate
+print(dictionary_restaurant)
+sorted_names = sorted(dictionary_restaurant.items())
+    
+for restaurant_name, rate in sorted_names:
+    print(f'{restaurant_name} is ratesd at {rate}.')
+
+
+
+
